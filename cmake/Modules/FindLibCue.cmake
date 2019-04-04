e7222880a907d01538cd775c11fadbcb6e6b6b35
Fernando Lemos <fernandotcl@gmail.com>
2019-04-04 19:10:18 +0200

    Fix finding libcue

--- cmake/Modules/FindLibCue.cmake
+++ cmake/Modules/FindLibCue.cmake
@@ -6,18 +6,18 @@
 #  LIBCUE_DEFINITIONS - Compiler switches required for using LibCue
 
 find_package(PkgConfig)
-pkg_check_modules(PC_LIBCUE QUIET libedit)
+pkg_check_modules(PC_LIBCUE QUIET libcue)
 set(LIBCUE_DEFINITIONS ${PC_LIBCUE_CFLAGS_OTHER})
 
-find_path(LIBCUE_INCLUDE_DIR libcue.h
+find_path(LIBCUE_INCLUDE_DIR libcue/libcue.h
           HINTS ${PC_LIBCUE_INCLUDEDIR} ${PC_LIBCUE_INCLUDE_DIRS}
-          PATH_SUFFIX libcue)
+          PATH_SUFFIXES libcue)
 
 find_library(LIBCUE_LIBRARY NAMES cue libcue
              HINTS ${PC_LIBCUE_LIBDIR} ${PC_LIBCUE_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibCue  DEFAULT_MSG LIBCUE_LIBRARY LIBCUE_INCLUDE_DIR)
+find_package_handle_standard_args(LibCue DEFAULT_MSG LIBCUE_LIBRARY LIBCUE_INCLUDE_DIR)
 
 mark_as_advanced(LIBCUE_INCLUDE_DIR LIBCUE_LIBRARY)
 
